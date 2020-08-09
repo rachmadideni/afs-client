caeb46d2b43747bf3e02809b1c84c2a26dd9cff0
Denny Rachmadi <denirachmadi82@gmail.com>
2020-08-10 01:49:13 +0800

    update styling tour

--- app/components/DialogTourStart/index.js
+++ app/components/DialogTourStart/index.js
@@ -4,12 +4,14 @@ import {
   DialogContent,
   DialogContentText,
   DialogActions,
-  Button,
   Slide,
-  Typography
+  Typography,
+  Avatar
 } from "@material-ui/core";
 
 import BtnCustom from "components/BtnCustom";
+import CSAvatar from "../../images/demimoore.jpg";
+import "./styles.scss";
 
 const Transition = React.forwardRef((props, ref) => (
   <Slide direction="down" ref={ref} {...props} />
@@ -20,15 +22,27 @@ const DialogTourStart = props => {
     <Dialog
       open={props.open}
       TransitionComponent={Transition}
-      onClose={props.onClose}>
-        <DialogContent>
-          <DialogContentText align="center">
-            <Typography variant="body2" align="center">
-            Hai selamat datang di AFS. apakah anda ingin menjalankan user guide?
-            </Typography>
-          </DialogContentText>
+      onClose={props.onClose}
+      className="dialog"
+      classes={{
+        paper:'dialogPaper'
+      }}>
+        <DialogContent className="dialogContent">
+          <div className="avatarWrapper">
+            <Avatar src={CSAvatar} />
+          </div>
+          <div>
+            <DialogContentText>
+              <Typography 
+                variant="body2" 
+                align="left" 
+                className="dialogTypography">
+                  Hai selamat datang di AFS. apakah anda ingin menjalankan user guide?
+              </Typography>
+            </DialogContentText>
+          </div>
         </DialogContent>
-        <DialogActions style={{ paddingRight:10,justifyContent:'center',alignItems: 'center', }}>
+        <DialogActions className="dialogActions">
           <BtnCustom 
             size="small"
             variant="outlined" 
--- app/components/ReactTour/index.js
+++ app/components/ReactTour/index.js
@@ -1,32 +1,75 @@
 import React from "react";
 import Tour from "reactour";
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Slide,
+  Typography,
+  Avatar,
+  Card,
+  CardContent,
+  CardActions  
+} from "@material-ui/core";
+
+import BtnCustom from "components/BtnCustom";
+import CSAvatar from "../../images/demimoore.jpg";
+import "./styles.scss";
 
 export const steps = [{
   selector:'[data-tut="tut-navigasi"]',
-  content:'ini adalah Tab menu navigasi'
+  content:'ini adalah Tab menu navigasi',
+  position:'top',
+  stepInteraction:false  
 },{
   selector:'[data-tut="tut-pengajuan"]',
-  content:'menu cepat untuk mengakses layanan AFS seperti pengajuan, informasi, dll. usap jari untuk berpindah menu dan klik tombol untuk mengakses layanan '
+  content:'ini adalah menu cepat untuk mengakses layanan di AFS. swipe untuk berpindah menu',
+  position:'top'
 },{
   selector:'[data-tut="tut-notif"]',
-  content:'ikon notifikasi digunakan untuk melihat pemberitahuan singkat selama menggunakan aplikasi'
+  content:'untuk melihat notifikasi singkat'
 },{
   selector:'[data-tut="tut-setting"]',
-  content:'klik disini untuk melihat dan merubah pengaturan'
+  content:'untuk merubah pengaturan aplikasi klik disini'
 }];
 
-const ReactTour = props => {
-  // console.log(props);    
+const ReactTour = props => {      
   return (
     <Tour 
       steps={steps} 
       isOpen={props.isOpen}       
       showNavigation={true}
       showButtons={true}
-      closeWithMask={false}
+      closeWithMask={true}
       showCloseButton={true}
+      disableInteraction      
+      CustomHelper={CustomHelper}
+      maskSpace={0}      
       {...props} />
   );
+  
+}
+
+const CustomHelper = ({ current, content, totalSteps, gotoStep, close }) => {
+  return (
+    <Card variant="outlined">
+      <CardContent className="cardContent">
+        <Avatar 
+          src={CSAvatar}
+          className="avatarBadge" />        
+        <Typography variant="body2">
+        {content}
+        </Typography>        
+      </CardContent>
+      <CardActions>
+        {current > 0 && <BtnCustom variant="outlined" size="small" color="primary" title="sebelumnya" onClick={()=>gotoStep(current-1)} />}
+        {current < totalSteps -1 && <BtnCustom variant="outlined" size="small" color="primary" title="selanjutnya" onClick={()=>gotoStep(current+1)} />}
+        {current === totalSteps -1 && <BtnCustom variant="outlined" size="small" color="primary" title="tutup" onClick={()=>close()}/>}
+        
+      </CardActions>
+    </Card>
+  )
 }
 
 export default ReactTour;
